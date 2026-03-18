73d2a43e334463f177fb10c9f18db6391d307178
NIl JOhn <canillasjohnlouie2007@gmail.com>
2026-03-18 12:35:22 +0800

    added new

---
+++ lowi.js
@@ -0,0 +1,12 @@
+
+    
+    let login = document.getElementById("login");
+
+    login.addEventListener("click", ()=>{
+        let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    alert("Wafoooo: "+ username);
+    alert("Password:"+ password);
+    })
+
+
